55c9e145db0ea96f4f919ccf28e05bfbf3c15381
zopeharshal <harshalzope99@gmail.com>
2024-07-08 14:52:20 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,42 +2,31 @@
 #include <cassert>
 using namespace std;
 
-// Pure functions for checking conditions
-bool isTemperatureOutOfRange(float temperature) {
-  return (temperature < 0 || temperature > 45);
+bool isOutOfRange(float value, float lowerBound, float upperBound) {
+  return (value < lowerBound || value > upperBound);
 }
 
-bool isSOCOutOfRange(float soc) {
-  return (soc < 20 || soc > 80);
-}
 
-bool isChargeRateOutOfRange(float chargeRate) {
-  return (chargeRate > 0.8);
-}
-
-// IO function to print error messages
 void printOutOfRangeMessage(const string& parameterName) {
   cout << parameterName << " out of range!\n";
 }
 
-// Function to check battery status using pure functions
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if (isTemperatureOutOfRange(temperature)) {
+  if (isOutOfRange(temperature, 0, 45)) {
     printOutOfRangeMessage("Temperature");
     return false;
   }
-  if (isSOCOutOfRange(soc)) {
+  if (isOutOfRange(soc, 20, 80)) {
     printOutOfRangeMessage("State of Charge");
     return false;
   }
-  if (isChargeRateOutOfRange(chargeRate)) {
+  if (isOutOfRange(chargeRate, 0, 0.8)) {
     printOutOfRangeMessage("Charge Rate");
     return false;
   }
   return true;
 }
 
-// Main function to demonstrate functionality with assertions
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
